4e81c0afabb0ddd7bb80e507636996afa2dcfa88
Dimas Panca Saputra <dimaspancas12@gmail.com>
2024-06-04 23:46:56 +0700

    Initialize

---
+++ tests/GroginStore.APITests/Controllers/PaymentControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace GroginStore.API.Controllers.Tests;
+
+public class PaymentControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> factory;
+
+    public PaymentControllerTests(WebApplicationFactory<Program> webApplicationFactory)
+    {
+        factory = webApplicationFactory;
+    }
+
+    [Fact]
+    public async Task GetAll_ForValidRequest_Returns200Ok()
+    {
+        // Arrange
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/api/Payment");
+
+        // Assert
+        //Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+    }
+}
